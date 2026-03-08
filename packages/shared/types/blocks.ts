8ace50bde12ee20b6877f659ad454a176df2aa4b
Kuro <hello@kurojifusky.com>
2026-03-08 09:22:44 +0800

    update svelte deps

--- packages/shared/types/blocks.ts
+++ packages/shared/types/blocks.ts
@@ -1,61 +1,58 @@
 interface BaseAttributes {
-	/** For generating UUID, fix by adding `crypto.randomUUID()` */
-	readonly uuid: ReturnType<typeof crypto.randomUUID>
-	locked?: {
-		value: boolean
-		byAuthor: string
-	}
+  /** For generating UUID, fix by adding `crypto.randomUUID()` */
+  readonly uuid: ReturnType<typeof crypto.randomUUID>
+  locked?: {
+    value: boolean
+    byAuthor: string
+  }
 }
 
 interface DialogBlock extends BaseAttributes {
-	type: "dialogue"
-	data: {
-		character: string
-		content: string
-	}
+  type: "dialogue"
+  data: {
+    character: string
+    content: string
+  }
 }
 
 interface NarratorBlock extends BaseAttributes {
-	type: "narrator"
-	data: {
-		content: string
-	}
+  type: "narrator"
+  data: {
+    content: string
+  }
 }
 
-interface SettingBlock extends BaseAttributes {
-	type: "setting"
-	data: {
-		environment: "interior" | "exterior"
-		time: string
-		location: string
-	}
+interface CueBlock extends BaseAttributes {
+  type: "cue"
+  data: {
+    environment: "interior" | "exterior"
+    time: string
+    location: string
+  }
 }
 
 interface SectionBlock extends BaseAttributes {
-	type: "section"
-	data: {
-		title: string
-		isChronological: true
-	}
+  type: "section"
+  data: {
+    title: string
+    isChronological: true
+  }
 }
 
 type ExtractKeyAsLiterals<K extends keyof I, I> = I extends {
-	[key in K]: infer R
+  [key in K]: infer R
 }
-	? R
-	: never
+  ? R
+  : never
 
-export type PlotBlock =
-	| DialogBlock
-	| NarratorBlock
-	| SettingBlock
-	| SectionBlock
+export type PlotBlock = DialogBlock | NarratorBlock | CueBlock | SectionBlock
+
 export type PlotBlockTypes = ExtractKeyAsLiterals<"type", PlotBlock>
 
 export interface CharacterItem extends BaseAttributes {
-	name: string
-	image: string
-	default: boolean
-	biography?: string
-	attributes?: Record<string, string>
+  name: string
+  image: string
+  default: boolean
+  biography?: string
+  attributes?: Record<string, string>
 }
